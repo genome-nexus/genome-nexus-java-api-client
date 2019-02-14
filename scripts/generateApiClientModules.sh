60d92b380f7b1c0c2ff7440c426e3ee09876200b
Hongxin Zhang <hongxin@cbio.mskcc.org>
2019-02-14 15:58:29 -0500

    Remove the client folder before generating the new ones

--- scripts/generateApiClientModules.sh
+++ scripts/generateApiClientModules.sh
@@ -11,6 +11,10 @@ if [ ! -f ${SWAGGER_CODEGEN_CLI_JAR} ]; then
     wget http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.3.1/swagger-codegen-cli-2.3.1.jar -O ${SWAGGER_CODEGEN_CLI_JAR}
 fi
 
+# remove the modules if available (otherwise some legacy methods in the test will not get removed)
+rm -rf ../genomeNexusPublicApiClient
+rm -rf ../genomeNexusInternalApiClient
+
 # generate java modules (see config json files for more details)
 java -jar ${SWAGGER_CODEGEN_CLI_JAR} generate -i ${GENOME_NEXUS_API_DOCS} -l java -c genomeNexusPublicApiClientConfig.json -o ../genomeNexusPublicApiClient
 java -jar ${SWAGGER_CODEGEN_CLI_JAR} generate -i ${GENOME_NEXUS_API_DOCS}?group=internal -l java -c genomeNexusInternalApiClientConfig.json -o ../genomeNexusInternalApiClient
